1b2fa174017de814904fd567122337895d8df5a5
blackgarlicbekasi-ship-it <blackgarlicbekasi@ship.it>
2026-03-22 09:29:13 +0000

    feat: setup admin dashboard at admin.ingat.cc
    - Add admin.ingat.cc to ADMIN_HOSTNAMES
    - Set DUB_WORKSPACE_ID to delaviva workspace (ws_1KMA2BND7BVHHG0FPQWVE6JY7)
    - Update Vercel env vars: NEXT_PUBLIC_APP_DOMAIN=ingat.cc, NEXTAUTH_URL=https://app.ingat.cc

--- packages/utils/src/constants/main.ts
+++ packages/utils/src/constants/main.ts
@@ -34,6 +34,7 @@ export const API_DOMAIN =
 
 export const ADMIN_HOSTNAMES = new Set([
   `admin.${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+  "admin.ingat.cc",
   "admin.localhost:8888",
   "admin.localhost",
 ]);
@@ -65,7 +66,7 @@ export const DUB_QR_LOGO = "https://assets.dub.co/logo.png";
 export const DUB_WORDMARK = "https://assets.dub.co/wordmark.png";
 export const DUB_THUMBNAIL = "https://assets.dub.co/thumbnail.jpg";
 
-export const DUB_WORKSPACE_ID = "cl7pj5kq4006835rbjlt2ofka";
+export const DUB_WORKSPACE_ID = "ws_1KMA2BND7BVHHG0FPQWVE6JY7";
 export const ACME_WORKSPACE_ID = "clrei1gld0002vs9mzn93p8ik";
 export const ACME_PROGRAM_ID = "prog_CYCu7IMAapjkRpTnr8F1azjN";
 export const LEGAL_WORKSPACE_ID = "clrflia0j0000vs7sqfhz9c7q";
