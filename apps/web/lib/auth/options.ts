87bb77bb32018684335006ec51299cefea84c825
blackgarlicbekasi-ship-it <blackgarlicbekasi@ship.it>
2026-03-22 05:59:32 +0000

    fix cookie domain for vercel.app subdomain

--- apps/web/lib/auth/options.ts
+++ apps/web/lib/auth/options.ts
@@ -337,7 +337,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT
+        domain: false && VERCEL_DEPLOYMENT
           ? `.${process.env.NEXT_PUBLIC_APP_DOMAIN}`
           : undefined,
         secure: VERCEL_DEPLOYMENT,
